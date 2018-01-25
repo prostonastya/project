28602208e14af27baf9d87e4564db06cac3704e3
prostonastya <find@li.ru>
2018-01-25 13:22:18 +0200

    dinamic render catalog and filter item

---
+++ js/dist/catalog_page.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var hidenMenu = document.getElementsByClassName('drop-hidden');
+var mainCatalog = document.getElementsByClassName('row')[1];
+// filter catalog item by category and fashion:
+var filteredCatalog = _.filter(window.catalog, { 'category': 'women', 'fashion': 'Casual style' });
+// sort by date(newest first):
+var sortDateOld = _.sortBy(filteredCatalog, ['dateAdded']);
+var sortDateNew = [];
+
+for (var i = 0; i < sortDateOld.length; i++) {
+    sortDateNew.unshift(sortDateOld[i]);
+}
+console.log(sortDateNew);
+
+// droppdown
+for (var _i = 0; _i < hidenMenu.length; _i++) {
+    hidenMenu[_i].addEventListener('click', selected);
+}
+
+function selected() {
+    var selectedVal = event.target.innerText;
+    var container = event.target.parentNode.parentNode; //li    
+    var value = container.getElementsByClassName('value')[0];
+    if (selectedVal == 'Not selected') {
+        value.innerText = '';
+    } else {
+        value.innerText = selectedVal;
+    }
+}
+
+// catalog item
+
+for (var _i2 = 0; _i2 < sortDateNew.length; _i2++) {
+    var item = document.createElement('div');
+    item.className = 'item';
+
+    var img = document.createElement('img');
+    img.src = sortDateNew[_i2].thumbnail;
+    img.alt = 'item-arrivals';
+    item.appendChild(img);
+
+    var hover = document.createElement('div');
+    hover.className = 'text-hover';
+    var link = document.createElement('a');
+    link.href = 'item.html';
+    link.innerText = 'View item';
+    hover.appendChild(link);
+    item.appendChild(hover);
+
+    var itemTitle = document.createElement('p');
+    itemTitle.innerText = sortDateNew[_i2].title;
+    item.appendChild(itemTitle);
+
+    var price = document.createElement('span');
+    if (sortDateNew[_i2].discountedPrice !== null) {
+        price.innerText = '\xA3' + sortDateNew[_i2].discountedPrice;
+    } else {
+        price.innerText = sortDateNew[_i2].placeholder;
+        price.classList.add('more');
+    }
+    if (sortDateNew[_i2].hasNew == true) {
+        price.classList.add('new-arrivals');
+    }
+
+    item.appendChild(price);
+    mainCatalog.appendChild(item);
+}
---
+++ js/src/catalog_page.js
@@ -0,0 +1,74 @@
+const hidenMenu = document.getElementsByClassName('drop-hidden');
+const mainCatalog =  document.getElementsByClassName('row')[1];
+// filter catalog item by category and fashion:
+const filteredCatalog = _.filter(window.catalog,{'category': 'women', 'fashion': 'Casual style'});
+// sort by date(newest first):
+var sortDateOld = _.sortBy(filteredCatalog, ['dateAdded']);
+var sortDateNew = [];
+
+for (let i=0;i < sortDateOld.length; i++){
+    sortDateNew.unshift(sortDateOld[i]);
+}
+console.log(sortDateNew);
+
+
+
+// droppdown
+for(let i=0; i<hidenMenu.length; i++){
+    hidenMenu[i].addEventListener('click', selected)
+}
+
+function selected(){    
+    var selectedVal = event.target.innerText;
+    var container = event.target.parentNode.parentNode;//li    
+    var value = container.getElementsByClassName('value')[0];    
+    if(selectedVal == 'Not selected'){
+        value.innerText = '';    
+    } else{
+        value.innerText = selectedVal;
+    }      
+}
+
+
+
+
+// catalog item
+
+for (let i=0; i< sortDateNew.length; i++){
+    let item = document.createElement('div');
+    item.className = 'item';
+
+    let img = document.createElement('img');
+    img.src = sortDateNew[i].thumbnail;
+    img.alt = 'item-arrivals';
+    item.appendChild(img);
+
+    let hover = document.createElement('div');
+    hover.className = 'text-hover';
+    let link = document.createElement('a');
+    link.href = 'item.html';
+    link.innerText = 'View item';    
+    hover.appendChild(link);
+    item.appendChild(hover);
+
+    let itemTitle = document.createElement('p');
+    itemTitle.innerText = sortDateNew[i].title;
+    item.appendChild(itemTitle);
+
+    let price = document.createElement('span');
+    if (sortDateNew[i].discountedPrice !== null){
+        price.innerText = `£${sortDateNew[i].discountedPrice}`;        
+    } else {
+        price.innerText = sortDateNew[i].placeholder;
+        price.classList.add('more');
+    }
+    if (sortDateNew[i].hasNew == true){
+        price.classList.add('new-arrivals');
+    }
+
+    item.appendChild(price);
+    mainCatalog.appendChild(item);
+}
+
+
+
